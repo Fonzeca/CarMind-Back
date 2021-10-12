77ae4611ea401977d20fc4ca82e42e45fdb33ec5
Daiko <daiko_011@hotmail.com>
2021-10-12 20:28:38 -0300

    lets begin

---
+++ src/main/java/com/mindia/carmind/CarMindApplication.java
@@ -0,0 +1,13 @@
+package com.mindia.carmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindia/carmind/CarMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindia.carmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
